d1ecd9ba9f6b3a67650466c848b9876cb55d6c87
Kevin Edwards <kedwards0107@gmail.com>
2021-05-04 14:39:11 -0400

    added database

--- sliders2.js
+++ sliders2.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Datastore = require('nedb');
 const app = express();
 app.listen(3000, () => console.log('listening at 3000'));
 app.use(express.static('public'));
@@ -7,13 +8,19 @@ app.use(express.json({ limit: "1mb" }));
 // app.get('/', function(req, res){
 //   res.send('hello world')
 // });
+const database = new Datastore('database.db');
+database.loadDatabase();
+
 
 app.post('/api', (request, response) => {
   console.log("I got a request!");
-  console.log(request.body);
   const data = request.body;
+  const timestamp = Date.now();
+  data.timestamp = timestamp;
+  database.insert(data);
   response.json({
     status: "success woohoo",
+    timestamp: timestamp,
     airflow: data.airflow
   });
 });
